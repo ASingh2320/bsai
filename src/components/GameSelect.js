86e6e382ba4878d9d8f733ab51664f3718580fab
Anmol Singh <john.doe@stonybrook.edu>
2021-05-27 16:40:54 -0400

    added select file

---
+++ src/components/GameSelect.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const GameSelect = (props) => {
+    return(
+        <div>
+        previous games, pending games, actions
+        </div>
+    );
+}
+
+export default GameSelect
